dad887b230a9df7e6d326c4d924422cb6d513c3a
Tiffany Tang <a00922762@se14l0124-15.ad.bcit.ca>
2018-02-13 17:01:03 -0800

    added

--- index.js
+++ index.js
@@ -3,25 +3,60 @@ const server = require("http").Server();
 
 var io =require("socket.io")(server);
 
-var allUsers = [];
+//var allUsers = [];
+
+var allusers1=[];
+var allusers2=[];
+
+var allusers={};
 
 io.on("connection", function(socket){
     console.log("connet");
-    allUsers.push(socket.id);
-    console.log(allUsers);
+//    allUsers.push(socket.id);
+//    console.log(allUsers);
     
-    socket.emit("yourid", socket.id);
+//    socket.emit("yourid", socket.id);
+//    
+//    io.emit("userJoined", allUsers);
     
-    io.emit("userJoined", allUsers);
+    socket.on("joinRoom", function(data){
+        console.log(data);
+        socket.join(data);
+        
+        socket.myRoom = data;
+        socket.emit("yourid", socket.id);
+        
+        if(!allusers[data]){
+            allusers[data]=[];
+        }
+        
+        allusers[data].push(socket.id);
+        io.to(data).emit("userJoined", allusers[data]);
+        
+//        if(data == "room1"){
+//            allusers1.push(socket.id);
+//             io.to(data).emit("userJoined", allusers1);
+//            console.log(allusers1);
+//        }else if(data == "room2"){
+//            allusers2.push(socket.id);
+//            io.to(data).emit("userJoined", allusers2);
+//            // to(data) tells which room it is. goes to the space first
+//        }
+    })
     
     socket.on("mymove", function(data){
-        socket.broadcast.emit("newmove", data)
+        socket.to(this.myRoom).emit("newmove", data)
     })
     
     socket.on("disconnect", function(){
-      var index = allUsers.indexOf(socket.id);
-        allUsers.splice(index, 1);
-        io.emit("userJoined", allUsers);
+        
+        var index = allusers[this.myRoom].indexOf(socket.id);
+        allusers[this.myRoom].splice(index, 1);
+        io.to(this.myRoom).emit("userJoined", allusers[this.myRoom]);
+        
+//      var index = allUsers.indexOf(socket.id);
+//        allUsers.splice(index, 1);
+//        io.emit("userJoined", allUsers);
     })
 });
 
